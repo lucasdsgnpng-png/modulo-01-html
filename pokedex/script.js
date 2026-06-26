383403fea60243b787dc337b1bf5e199d32048e1
lucasdsgnpng-png <lucasdsgn.png@gmail.com>
2026-06-26 17:43:43 -0300

    função de cor de tipo

--- pokedex/script.js
+++ pokedex/script.js
@@ -3,6 +3,22 @@ const imagemPokemon = document.querySelector('img')
 const pokemon = document.querySelector('#nome-pokemon')
 const tipo1 = document.querySelector('#tipo1')
 const tipo2 = document.querySelector('#tipo2')
+
+const cores = {
+    fire: '#ff6b35',
+  water: '#4fc3f7',
+  grass: '#81c784',
+  electric: '#ffd54f',
+  poison: '#6b1593',
+  flying:'#4f757f'
+}
+
+function corDoTipo(tipo){
+    const cor = cores[tipo]
+    return cor
+}
+
+
 async function buscarPokemon(nome) {
             const resposta = await fetch(`https://pokeapi.co/api/v2/pokemon/${nome}`)
             const dados = await resposta.json()
@@ -16,7 +32,9 @@ nomePokemon.addEventListener('keydown', async function(evento) {
        imagemPokemon.src = dados.sprites.front_default
        pokemon.textContent = dados.name
        tipo1.textContent = dados.types[0].type.name
+       tipo1.style.backgroundColor = corDoTipo(dados.types[0].type.name)
        tipo2.textContent = dados.types[1].type.name
+       tipo2.style.backgroundColor = corDoTipo(dados.types[1].type.name)
     }
 })
 
